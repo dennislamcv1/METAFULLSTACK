4a9d639de7c69db2cfff85fc321272cba48f5c29
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2026-09-24 14:38:54 +0800

    Add files via upload

--- Programming in Python/Functions, loops and data structures/ordering_system.py
+++ Programming in Python/Functions, loops and data structures/ordering_system.py
@@ -1,98 +1,84 @@
 menu = {
-    1: {"name": 'espresso',
-        "price": 1.99},
-    2: {"name": 'coffee', 
-        "price": 2.50},
-    3: {"name": 'cake', 
-        "price": 2.79},
-    4: {"name": 'soup', 
-        "price": 4.50},
-    5: {"name": 'sandwich',
-        "price": 4.99}
+    1: {"name": 'espresso', "price": 1.99},
+    2: {"name": 'coffee', "price": 2.50},
+    3: {"name": 'cake', "price": 2.79},
+    4: {"name": 'soup', "price": 4.50},
+    5: {"name": 'sandwich', "price": 4.99}
 }
 
 def calculate_subtotal(order):
-    """ Calculates the subtotal of an order
-
-    [IMPLEMENT ME] 
-        1. Add up the prices of all the items in the order and return the sum
-
-    Args:
-        order: list of dicts that contain an item name and price
-
-    Returns:
-        float = The sum of the prices of the items in the order
-    """
-    print('Calculating bill subtotal...')
-    ### WRITE SOLUTION HERE
-    addUp = sum(d['price'] for d in order if d)
-    return round(addUp, 2)
-
-    raise NotImplementedError()
-
-def calculate_tax(subtotal):
-    """ Calculates the tax of an order
-
-    [IMPLEMENT ME] 
-        1. Multiply the subtotal by 15% and return the product rounded to two decimals.
-
-    Args:
-        subtotal: the price to get the tax of
-
-    Returns:
-        float - The tax required of a given subtotal, which is 15% rounded to two decimals.
-    """
-    print('Calculating tax from subtotal...')
-    ### WRITE SOLUTION HERE
-    tax = (subtotal * 0.15)
-    return round(tax, 2)
-
-    raise NotImplementedError()
-
-def summarize_order(order):
-    """ Summarizes the order
+    """Calculates the subtotal of an order.
 
     [IMPLEMENT ME]
-        1. Calculate the total (subtotal + tax) and store it in a variable named total (rounded to two decimals)
-        2. Store only the names of all the items in the order in a list called names
-        3. Return names and total.
+        1. Add up the prices of all items in the order.
+        2. Round the result to 2 decimal places and return it.
 
     Args:
-        order: list of dicts that contain an item name and price
+        order (list): A list of dicts, where each dict represents an item with a name and price.
 
     Returns:
-        tuple of names and total. The return statement should look like 
-        
-        return names, total
+        float: The subtotal of all item prices, rounded to 2 decimal places.
+    """
+    print('Calculating bill subtotal...')
+    subtotal = sum(item["price"] for item in order)
+    return round(subtotal, 2)
 
+
+def calculate_tax(subtotal):
+    """Calculates the tax of an order.
+
+    [IMPLEMENT ME]
+        1. Calculate 15% of the subtotal.
+        2. Round the tax to 2 decimal places and return it.
+
+    Args:
+        subtotal (float): The subtotal amount.
+
+    Returns:
+        float: The calculated tax amount, rounded to 2 decimal places.
+    """
+    print('Calculating tax from subtotal...')
+    tax = subtotal * 0.15
+    return round(tax, 2)
+
+
+def summarize_order(order):
+    """Summarizes the order.
+
+    [IMPLEMENT ME]
+        1. Calculate subtotal and tax.
+        2. Compute the total (subtotal + tax) and round to 2 decimal places.
+        3. Extract the item names and return them with the total.
+
+    Args:
+        order (list): A list of dicts, where each dict represents an item with a name and price.
+
+    Returns:
+        tuple: A list of item names and the total amount (rounded to 2 decimal places).
     """
     print_order(order)
-    ### WRITE SOLUTION HERE
     subtotal = calculate_subtotal(order)
     tax = calculate_tax(subtotal)
-    total = round((subtotal + tax), 2)
-    names = []
-    for x in order:
-        names.append(x['name'])
+    total = round(subtotal + tax, 2)
+    names = [item["name"] for item in order]
     return names, total
-    raise NotImplementedError()
 
-# This function is provided for you, and will print out the items in an order
+
+# This function is provided for you and will print out the items in an order
 def print_order(order):
     print('You have ordered ' + str(len(order)) + ' items')
-    items = []
     items = [item["name"] for item in order]
     print(items)
     return order
 
-# This function is provided for you, and will display the menu
+# This function is provided for you and will display the menu
 def display_menu():
     print("------- Menu -------")
     for selection in menu:
         print(f"{selection}. {menu[selection]['name'] : <9} | {menu[selection]['price'] : >5}")
     print()
 
-# This function is provided for you, and will create an order by prompting the user to select menu items
+# This function is provided for you and will create an order by prompting the user to select menu items
 def take_order():
     display_menu()
     order = []
@@ -111,13 +97,14 @@ def main():
     order = take_order()
     print_order(order)
 
-    # subtotal = calculate_subtotal(order)
-    # print("Subtotal for the order is: " + str(subtotal))
+    subtotal = calculate_subtotal(order)
+    print("Subtotal for the order is: " + str(subtotal))
 
-    # tax = calculate_tax(subtotal)
-    # print("Tax for the order is: " + str(tax))
+    tax = calculate_tax(subtotal)
+    print("Tax for the order is: " + str(tax))
 
-    # items, subtotal = summarize_order(order)
+    names, total = summarize_order(order)
+    print(f"Order summary: Items: {names}, Total: {total}")
 
 if __name__ == "__main__":
     main()
